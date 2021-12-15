b8261b9f0ce813c8dc6490c0016b3a83ba8eae6d
Vika-star <72336972+Vika-star@users.noreply.github.com>
2021-12-15 12:37:12 +0300

    Preloader changing

--- my-app/src/components/Preloader/Preloader.jsx
+++ my-app/src/components/Preloader/Preloader.jsx
@@ -4,7 +4,9 @@ import style from './style.module.scss'
 const Preloader = () => {
     return (
         <div className={style.preloader}>
-            <CircularProgress color="#FF0000" />
+            <div className={style.preloader__block}>
+                <CircularProgress color="#FF0000" className={style.preloader__circle}/>
+            </div>
         </div>
     )
 }
